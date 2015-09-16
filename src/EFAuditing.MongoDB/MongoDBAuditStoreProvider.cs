4492052fdb04fd1dd64f2c1dcadea2b20ae85164
Johann Brink <johann.brink@outlook.com>
2015-09-16 18:00:05 +0200

    Added Mongo project

---
+++ src/EFAuditing.MongoDB/MongoDBAuditStoreProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace EFAuditing.MongoDB
+{
+    public class MongoDBAuditStoreProvider : IExternalAuditStoreProvider
+    {
+        private readonly string _connectionString;
+
+        public MongoDBAuditStoreProvider(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<AuditLog> ReadAuditLogs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async void WriteAuditLogsAsync(IEnumerable<AuditLog> auditLogs)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("foo");
+            var collection = database.GetCollection<BsonDocument>("bar");
+            throw new NotImplementedException();
+        }
+    }
+}
