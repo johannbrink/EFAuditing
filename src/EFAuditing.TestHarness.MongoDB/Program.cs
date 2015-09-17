b1047bb9a854b2863b1d394f7fd8baa205a7ea2b
Johann Brink <johann.brink@outlook.com>
2015-09-17 10:36:35 +0200

    test

---
+++ src/EFAuditing.TestHarness.MongoDB/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Reflection;
+using EFAuditing.MongoDB;
+using EFAuditing.TestHarness.Shared;
+
+namespace EFAuditing.TestHarness.MongoDB
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            var mongoDBProvider = new MongoDBAuditStoreProvider("mongodb://localhost", "TestLogging","AuditLogs");
+            using (var myDbContext = new MyDBContext())
+            {
+                myDbContext.Database.EnsureDeleted();
+                myDbContext.Database.EnsureCreated();
+
+                var customer = new Customer()
+                {
+                    FirstName = "TestFirstName",
+                    LastName = "TEstLAstNAme"
+                };
+                myDbContext.Customers.Add(customer);
+
+                var auditablePropCount =
+                    customer.GetType()
+                        .GetProperties()
+                        .Count(p => !p.GetCustomAttributes(typeof(DoNotAudit), true).Any());
+
+                var nonAuditablePropCount =
+                    customer.GetType()
+                        .GetProperties()
+                        .Count(p => p.GetCustomAttributes(typeof(DoNotAudit), true).Any());
+                myDbContext.SaveChanges("Test User");
+
+                Console.WriteLine($"Added object with {auditablePropCount} auditable properties and {nonAuditablePropCount} non-auditable properties.");
+
+                var auditLogs = myDbContext.GetAuditLogs().ToList();
+                Console.WriteLine($"Audit log contains {auditLogs.Count()} entries.");
+                foreach (var auditLog in myDbContext.GetAuditLogs())
+                {
+                    Console.WriteLine($"AuditLogId:{auditLog.AuditLogId} TableName:{auditLog.TableName} ColumnName:{auditLog.ColumnName} OriginalValue:{auditLog.OriginalValue} NewValue:{auditLog.NewValue} EventDateTime:{auditLog.EventDateTime}");
+                }
+
+                if (auditLogs.Count() == auditablePropCount)
+                    Console.WriteLine("Test succeeded.");
+                else
+                    throw new Exception("Something is wrong.");
+
+                Console.Read();
+                myDbContext.Database.EnsureDeleted();
+            }
+        }
+    }
+}
